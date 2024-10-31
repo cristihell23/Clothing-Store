95528990bd6176c38ea5834aa86c5dab7e1dc3fc
Cristian H <cristihell23@yahoo.com>
2024-10-31 19:04:57 +0000

    Database Function Updated
    get all products function created - function returns products as dictionary for ease of use. Comments were also added.

--- clothing_app/backend/products_dao.py
+++ clothing_app/backend/products_dao.py
@@ -1,16 +1,46 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root',password='root',
-                              host='127.0.0.1',
-                              database='cs')
-
-cursor = cnx.cursor()
-
-query = "SELECT * FROM cs.products"
-
-cursor.execute(query)
-
-for (product_id, product_name, uom_id, product_size_id, product_color_id, price_per_item) in cursor :
-    print(product_id, product_name, uom_id, product_size_id, product_color_id, price_per_item)
+def get_all_products():
     
-cnx.close()
+    # Creating Database Connection
+    cnx = mysql.connector.connect(user='root',password='root',
+                                host='127.0.0.1',
+                                database='cs')
+
+    #Creating Database Cursor
+    cursor = cnx.cursor()
+
+    #Database Query - To Be Amended upon Database Update
+    query = ("SELECT products.product_id, products.product_name, products.uom_id, uom.uom_name, products.product_size_id, size.size_name, products.product_color_id, color.color_name,products.price_per_item FROM products INNER JOIN uom ON products.uom_id = uom.uom_id INNER JOIN size ON products.product_size_id = size.size_id INNER JOIN color ON products.product_color_id = color.color_id;")
+
+    #Cursor Execution
+    cursor.execute(query)
+
+    #List of Dictionary containing all products info's - We are using Dictionary so we can extract certain informations easier
+    response = []
+
+    #Loop that gets all relevant product info from database 
+    for (product_id, product_name, uom_id, uom_name, product_size_id, size_name, product_color_id, color_name, price_per_item) in cursor :
+        response.append(
+            {
+                'product_id' : product_id,
+                'product_name' : product_name,
+                'uom_id' : uom_id,
+                'uom_name' : uom_name,
+                'product_size_id' : product_size_id,
+                'size_name' : size_name,
+                'product_color_id' : product_color_id,
+                'color_name' : color_name,
+                'price_per_item' : price_per_item
+            }
+        )
+
+        #print(product_id, product_name, uom_id, uom_name, product_size_id, size_name, product_color_id, color_name, price_per_item) - This method was used for testing purposes as it only prints the results.
+
+    #Closing Connection
+    cnx.close()
+
+    return response
+
+if __name__ == '__main__':
+    print(get_all_products())
