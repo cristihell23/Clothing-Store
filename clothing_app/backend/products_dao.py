538f398231881f5eb464c900fa766d8720053f72
Cristian H <cristihell23@yahoo.com>
2024-11-01 13:35:08 +0000

    Separating SQL Connection in a different file
    Separating SQL in a different file to make the code more modular and easier to manage.

--- clothing_app/backend/products_dao.py
+++ clothing_app/backend/products_dao.py
@@ -1,14 +1,9 @@
-import mysql.connector
+from sql_connection import get_sql_connection
 
-def get_all_products():
+def get_all_products(connection):
     
-    # Creating Database Connection
-    cnx = mysql.connector.connect(user='root',password='root',
-                                host='127.0.0.1',
-                                database='cs')
-
     #Creating Database Cursor
-    cursor = cnx.cursor()
+    cursor = connection.cursor()
 
     #Database Query - To Be Amended upon Database Update
     query = ("SELECT products.product_id, products.product_name, products.uom_id, uom.uom_name, products.product_size_id, size.size_name, products.product_color_id, color.color_name,products.price_per_item FROM products INNER JOIN uom ON products.uom_id = uom.uom_id INNER JOIN size ON products.product_size_id = size.size_id INNER JOIN color ON products.product_color_id = color.color_id;")
@@ -38,9 +33,10 @@ def get_all_products():
         #print(product_id, product_name, uom_id, uom_name, product_size_id, size_name, product_color_id, color_name, price_per_item) - This method was used for testing purposes as it only prints the results.
 
     #Closing Connection
-    cnx.close()
+    connection.close()
 
     return response
 
 if __name__ == '__main__':
-    print(get_all_products())
+    connection = get_sql_connection()
+    print(get_all_products(connection))
---
+++ clothing_app/backend/sql_connection.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+# Creating Local Variable
+__cnx = None
+
+# Defining SQL Connection
+def get_sql_connection():
+    # Making the local variable Global
+    global __cnx
+    if __cnx is None:
+    # Creating Database Connection
+        __cnx = mysql.connector.connect(user='root',password='root',
+                                host='127.0.0.1',
+                                database='cs')
+    return __cnx
