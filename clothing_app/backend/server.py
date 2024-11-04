4a094eb7550cdb772caac28d08351599759457c0
Cristian H <cristihell23@yahoo.com>
2024-11-04 16:36:48 +0000

    getProduct Function implemented
    Gets all products and return them as JSON.

--- clothing_app/backend/server.py
+++ clothing_app/backend/server.py
@@ -1,13 +1,30 @@
 from flask import Flask, request, jsonify
+from sql_connection import get_sql_connection
+import products_dao
 
 
 app = Flask (__name__)
 
+connection = get_sql_connection()
+
 
 # Use this to test server - 127.0.0.1:5000/hello
 @app.route('/hello')
+
+# Test Function
 def hello():
     return "hello !"
+
+# For easy of use copy - 127.0.0.1:5000/getProducts
+@app.route('/getProducts', methods = ['GET'])
+
+# Get all products from database and return them as JSON.
+def get_Products():
+    products = products_dao.get_all_products(connection)
+    response = jsonify(products)
+    # CORS documentation - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
 if __name__ == "__main__":
     print("Starting Flask Server")
